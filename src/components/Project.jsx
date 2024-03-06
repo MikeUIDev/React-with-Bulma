71f2597cd1957afe7cd132733ab26c1265c85d73
Mike Lin <mlindesign@gmail.com>
2024-03-06 15:57:16 -0800

    added work projects component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,18 +4,25 @@ import { faExternalLink } from '@fortawesome/free-solid-svg-icons'
 
 
 function Job(props) {
+  const noLink = {
+    display: "none"
+  };
+
   return (
     <div className="columns content is-medium mb-6">
       <div className="column is-12">
         {/* Card */}
         <div className="card">
           <div className="card-image">
-              <img src={props.img} alt={props.name} title={props.name}></img>
+            <video loading="lazy" playsInline muted controls className="video" poster={props.videoPoster} width="612" height="413">
+              <source type="video/mp4" src={props.videoUrl}></source>
+            </video>
+              {/* <img src={props.img} alt={props.name} title={props.name}></img> */}
           </div>
           <footer className="card-footer mt-4">
             <div className="card-footer-item mb-0 is-flex is-align-items-flex-start is-justify-content-flex-start is-flex-direction-column">
-              <p className="">
-                <a href={props.link} alt={props.name} title={props.name} target="_blank">{props.name} &nbsp; <FontAwesomeIcon icon={faExternalLink} color="#ffffff" size="xs"></FontAwesomeIcon> </a>    
+              <p className="link-width">
+                <a href={props.link} alt={props.name} title={props.name} target="_blank">{props.name} &nbsp; <FontAwesomeIcon className="fa-icon" icon={faExternalLink} size="xs"></FontAwesomeIcon> </a>
               </p>
               <p className="tag is-dark is-medium has-text-white">{props.type}</p>
             </div>
--- src/components/pages/Work.jsx
+++ src/components/pages/Work.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import Project from "../Project"
+import Video from "../Video"
 import Nissan from "../../assets/images/Nissan-Heisman-House-ESPN.jpg"
+import DisneyAsia from "../../assets/images/Home-Disney-Asia-Advertising-Sales.jpg"
+import CreativeWorks from "../../assets/images/Home-ESPN-CreativeWorks.jpg"
+import AllState from "../../assets/images/Allstate-AFCA-Good-Works-Team-ESPN.jpg"
+
+import NissanVideo from "../../assets/videos/Nissan-Heisman-House-ESPN.mp4"
+import DisneyAsiaVideo from "../../assets/videos/Home-Disney-Asia-Advertising-Sales.mp4"
+import CreativeWorksVideo from "../../assets/videos/Home-ESPN-CreativeWorks.mp4"
+import AllStateVideo from "../../assets/videos/Allstate-AFCA-Good-Works-Team-ESPN.mp4"
+
+
 
 function Work() {
   return (
@@ -23,7 +34,8 @@ function Work() {
             <div className="columns">
               <div className="column is-6">
                 <Project
-                  img={Nissan}
+                  videoPoster ={Nissan}
+                  videoUrl = {NissanVideo}
                   name="Nissan Heisman House"
                   type="Heroku"
                   link="https://promo.espn.com/espn/contests/nissan/heisman/2023/index"
@@ -32,10 +44,33 @@ function Work() {
               </div>
               <div className="column is-6">
                 <Project
-                  img={Nissan}
-                  name="Disney Asia"
+                  videoPoster ={AllState}
+                  videoUrl = {AllStateVideo}
+                  name="Allstate AFCA Good Works Team"
+                  type="Heroku"
+                  link="https://promo.espn.com/espn/contests/allstate/2023/index"
+                  description="A promotion campaign which fans can vote their favorite college football players in the United States."
+                />
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column is-6">
+                <Project
+                  videoPoster ={CreativeWorks}
+                  videoUrl = {CreativeWorksVideo}
+                  name="ESPN CreativeWorks"
                   type="Contentful CMS"
-                  link="https://mikelinstudio.com"
+                  link="https://showcase.espncreativeworks.com/"
+                  description="A promotion campaign associated with the Heisman Trophy, which fans can vote their favorite college football players in the United States."
+                />
+              </div>
+              <div className="column is-6">
+                <Project
+                  videoPoster ={DisneyAsia}
+                  videoUrl = {DisneyAsiaVideo}
+                  name="Disney Asia Advertising Sales"
+                  type="Contentful CMS"
+                  link="https://mikelinstudio.com/images/Home-Disney-Asia-Advertising-Sales.jpg"
                   description="Showcase Disney campaigns in an Asia territory."
                 />
               </div>
