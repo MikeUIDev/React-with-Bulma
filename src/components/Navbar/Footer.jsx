744a97011000464acb51a91b14f05a0d58934066
Mike Lin <mlindesign@gmail.com>
2024-07-01 17:17:09 -0700

    updated footer with Upwork link

--- src/components/Navbar/Footer.jsx
+++ src/components/Navbar/Footer.jsx
@@ -27,19 +27,22 @@ function Footer() {
             <p className="is-size-6-touch has-text-weight-normal text-center-mobile has-text-left-tablet has-text-left-desktop">mlindesign&#64;gmail&#46;com</p>
           </div> */}
           <div className="column social-icons is-one-quarter is-flex is-justify-content-space-evenly">
-            <a className="linkedin" href="https://www.linkedin.com/in/mikelindesign/" target="_blank" rel="noreferrer" alt="Mike LinkedIn">
+            <a className="upwork" href="https://www.upwork.com/freelancers/~01192110ffbde9b4f7?mp_source=share" target="_blank" rel="noreferrer" alt="Upwork Mike Lin Visual Designer" title="Upwork Mike Lin Visual Designer">
+              <FontAwesomeIcon icon={['fab', 'upwork']} color="rgba(246, 242, 229, 0.8)" size="2x" />
+            </a>
+            <a className="linkedin" href="https://www.linkedin.com/in/mikelindesign/" target="_blank" rel="noreferrer" alt="Mike Lin LinkedIn" title="Mike Lin LinkedIn">
               <FontAwesomeIcon icon={['fab', 'linkedin']} color="rgba(246, 242, 229, 0.8)" size="2x" />
             </a>
-            <a className="github" href="https://github.com/MikeUIDev" target="_blank" rel="noreferrer" alt="Mike GitHub">
+            <a className="github" href="https://github.com/MikeUIDev" target="_blank" rel="noreferrer" alt="Mike Lin GitHub" title="Mike Lin GitHub">
               <FontAwesomeIcon icon={['fab', 'github']} color="rgba(246, 242, 229, 0.8)" size="2x" />
             </a>
-            <a className="dribbble" href="https://dribbble.com/mikelindesign" target="_blank" rel="noreferrer" alt="Mike Dribbble">
+            <a className="dribbble" href="https://dribbble.com/mikelindesign" target="_blank" rel="noreferrer" alt="Mike Lin Dribbble" title="Mike Lin Dribbble">
               <FontAwesomeIcon icon="fa-brands fa-dribbble" color="rgba(246, 242, 229, 0.8)" size="2x" />
             </a>
-            <a className="youtube" href="https://www.youtube.com/channel/UCmCK0FAXOLkXB3A6w3w9mIA" target="_blank" rel="noreferrer" alt="YouTube Music Channel Urban Lofi Studio">
+            <a className="youtube" href="https://www.youtube.com/channel/UCmCK0FAXOLkXB3A6w3w9mIA" target="_blank" rel="noreferrer" alt="YouTube Music Channel Urban Lofi Studio" title="YouTube Music Channel Urban Lofi Studio">
               <FontAwesomeIcon icon="fa-brands fa-youtube" color="rgba(246, 242, 229, 0.8)" size="2x" />
             </a>
-            <a id="myEmail" className="email" href="mailto:mike&#64;mikelinstudio&#46;com" target="_blank" rel="noreferrer" alt="Mike Email">
+            <a id="myEmail" className="email" href="mailto:mike&#64;mikelinstudio&#46;com" target="_blank" rel="noreferrer" alt="Mike Lin Email" title="Mike Lin Email">
               <FontAwesomeIcon icon={faEnvelope} color="rgba(246, 242, 229, 0.8)" size="2x"></FontAwesomeIcon>
             </a>
           </div>
